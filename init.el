23761c291621463812927075946770363d26c6fc
Gilgamesh <ddevnich@ucmerced.edu>
2017-01-28 16:13:41 -0800

    Notes on managing vendor branches

--- init.el
+++ init.el
@@ -65,8 +65,8 @@
 (require 'init-smex)
 ;; If you really prefer ido to ivy, change the comments below. I will
 ;; likely remove the ido config in due course, though.
-;; (require 'init-ido)
-(require 'init-ivy)
+(require 'init-ido)
+;; (require 'init-ivy)
 (require 'init-hippie-expand)
 (require 'init-company)
 (require 'init-windows)
