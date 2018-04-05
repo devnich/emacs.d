180e706d85bbabe57549203649a1f0fae98f12c1
Gilgamesh <ddevnich@ucmerced.edu>
2018-04-05 15:49:22 -0700

    Send some keys directly to terminal process

--- lisp/init-multi-term.el
+++ lisp/init-multi-term.el
@@ -3,10 +3,22 @@
 
 ;;; Use Fish Shell in multi-term
 (setq multi-term-buffer-name "~fish")
-(setq multi-term-program "/usr/bin/fish")       ;; use fish in terminal
+(setq multi-term-program "/usr/bin/fish")
 
-;;; Keyboard shortcuts
-(global-set-key (kbd "C-c T") 'multi-term)      ;; create new terminal buffer
-(global-set-key (kbd "C-c t") 'multi-term-next) ;; switch terminals
+;;; Create new terminal buffer
+(global-set-key (kbd "C-c T") 'multi-term)
+
+;;; Switch to next terminal buffer
+(global-set-key (kbd "C-c t") 'multi-term-next)
+
+;; Allow some keys to bypass Emacs and go to the underlying term process
+(add-hook 'term-mode-hook
+          (lambda () (define-key term-raw-map (kbd "ESC") 'term-send-esc)))
+(add-hook 'term-mode-hook
+          (lambda () (define-key term-raw-map (kbd "C-y") 'term-paste)))
+
+;;; Define keys to toggle shell modes
+(add-to-list 'term-bind-key-alist '("C-c C-j" . term-line-mode))
+(add-to-list 'term-bind-key-alist '("C-c C-k" . term-char-mode))
 
 (provide 'init-multi-term)
