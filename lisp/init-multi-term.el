cc7335c7c72a81da6e3ad62766ef93f7b83ebb87
Gilgamesh <ddevnich@ucmerced.edu>
2018-04-06 14:07:31 -0700

    Term mode toggle

--- lisp/init-multi-term.el
+++ lisp/init-multi-term.el
@@ -16,9 +16,7 @@
           (lambda () (define-key term-raw-map (kbd "ESC") 'term-send-esc)))
 (add-hook 'term-mode-hook
           (lambda () (define-key term-raw-map (kbd "C-y") 'term-paste)))
-
-;;; Define keys to toggle shell modes
-(add-to-list 'term-bind-key-alist '("C-c C-j" . term-line-mode))
-(add-to-list 'term-bind-key-alist '("C-c C-k" . term-char-mode))
+(add-hook 'term-mode-hook
+          (lambda () (define-key term-raw-map (kbd "C-c C-j") 'term-line-mode)))
 
 (provide 'init-multi-term)
