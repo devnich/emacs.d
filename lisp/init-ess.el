b0cb3d9df20d0ce66e8e47231d55e5ebf2873bb2
Gilgamesh <devnich@gmail.com>
2022-07-12 22:06:40 -0700

    Syntax highlighting for RMarkdown files with polymode

--- lisp/init-ess.el
+++ lisp/init-ess.el
@@ -2,9 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (maybe-require-package 'ess))
+(when (maybe-require-package 'ess)
+  (add-to-list 'auto-mode-alist '("\\.R\\'" . ess-r-mode)))
 
-(add-to-list 'auto-mode-alist '("\\.R\\'" . ess-r-mode))
+(maybe-require-package 'polymode)
+(when (maybe-require-package 'poly-R)
+  ;; associate the new polymode to Rmd files:
+  (add-to-list 'auto-mode-alist
+               '("\\.[rR]md\\'" . poly-gfm+r-mode))
+  ;; uses braces around code block language strings:
+  (setq markdown-code-block-braces t))
 
 (provide 'init-ess)
 ;;; init-ess.el ends here
