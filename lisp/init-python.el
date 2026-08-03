c252f640e4d9d68c9b1b8bf7069fe01dd4a683a4
Gilgamesh <devnich@gmail.com>
2026-08-03 14:16:29 -0700

    Update python config for miniforg

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -12,7 +12,7 @@
                 ("SConscript\\'" . python-mode))
               auto-mode-alist))
 
-(setq python-shell-interpreter "python3")
+;; (setq python-shell-interpreter "python3")
 
 (require-package 'pip-requirements)
 
@@ -30,22 +30,30 @@
 
 
 ;;; Automatic support for Conda virtual environments (DD)
+;;; To manually activate or deactivate a conda environment:
+;;;   M-x conda-env-activate
+;;;   M-x conda-env-deactivate
 (when (maybe-require-package 'conda)
   ;; Find Anaconda and environments on MacOS
   (when *is-a-mac*
-    (setq conda-anaconda-home (expand-file-name "~/opt/anaconda3"))
-    (setq conda-env-home-directory (expand-file-name "~/opt/anaconda3")))
+    (setq conda-env-home-directory (expand-file-name "~/miniforge3")))
 
   ;; if you want interactive shell support, include:
   (conda-env-initialize-interactive-shells)
 
   ;; if you want eshell support, include:
-  ;; (conda-env-initialize-eshell)
+  (conda-env-initialize-eshell)
 
   ;; Auto-activate environment if we find an environment.yml file. This produces
   ;; annoying message spam every time a file opens:
   ;; (conda-env-autoactivate-mode t)
   )
+
+;;; New versions of Python look for pyrepl, which doesn't work in the current
+;;; Emacs run-python (DD)
+(with-eval-after-load 'python
+  (setenv "PYTHON_BASIC_REPL" "1"))
+
 
 (provide 'init-python)
 ;;; init-python.el ends here
